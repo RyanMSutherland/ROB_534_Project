0032597eb24d430c11c66ead59e6a834f3f37cfd
Ryan Sutherland <Ryan.Suth01@gmail.com>
2025-02-17 12:21:15 -0800

    Saving

--- harvest_control/src/move_arm.cpp
+++ harvest_control/src/move_arm.cpp
@@ -223,8 +223,8 @@ void MoveArmNode::move_to_pose(const std::shared_ptr<harvest_interfaces::srv::Mo
     // Plan and execute
     start_time = rclcpp::Time now = this->get_clock()->now();
     moveit::planning_interface::MoveGroupInterface::Plan goal;
-    std::cout << "Total time to compute path: " << start_time-this->get_clock()->now(); << std::endl;
-    
+    std::cout << "Total time to compute path: " << this->get_clock()->now() - start_time; << std::endl;
+
     if (move_group_.plan(goal))
     {
         // this->move_group_.execute(goal);
